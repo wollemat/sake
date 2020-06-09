a864a2f028b1c979a5c05f57306069aa809d936a
wollemat <f.c.slothouber@gmail.com>
2020-06-09 17:24:22 +0200

    Setup Gradle build system

---
+++ src/main/kotlin/me/wollemat/SakeLoader.kt
@@ -0,0 +1,5 @@
+package me.wollemat
+
+fun main() {
+    println("Hello, World!")
+}
---
+++ src/test/kotlin/me/wollemat/SakeTest.kt
@@ -0,0 +1,12 @@
+package me.wollemat
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SakeTest {
+
+    @Test
+    fun `simple unit test`() {
+        assertEquals(1, 1)
+    }
+}
