aa4765f2ac936545bf4f62f3e1df0b74f16fb396
wollemat <f.c.slothouber@gmail.com>
2020-06-10 00:18:06 +0200

    Setup ANTLR lexer and parser generation

---
+++ src/main/antlr/me/wollemat/parser/Hello.g4
@@ -0,0 +1,9 @@
+grammar Hello;
+
+@header {
+package me.wollemat.parser;
+}
+
+start  : 'hello' ID ;       // match keyword hello followed by an identifier
+ID : [a-z]+ ;               // match lower-case identifiers
+WS : [ \t\r\n]+ -> skip ;   // skip spaces, tabs, newlines
--- src/main/kotlin/me/wollemat/SakeLoader.kt
+++ src/main/kotlin/me/wollemat/SakeLoader.kt
@@ -1,5 +1,11 @@
 package me.wollemat
 
+import me.wollemat.parser.HelloLexer
+import me.wollemat.parser.HelloParser
+import org.antlr.v4.runtime.ANTLRInputStream
+import org.antlr.v4.runtime.CommonTokenStream
+
 fun main() {
-    println("Hello, World!")
+    val parser = HelloParser(CommonTokenStream(HelloLexer(ANTLRInputStream("hello hans"))))
+    println(parser.start().ID())
 }
