dda1d18ee49bfaa07e468b2daf13063c2142569f
wollemat <f.c.slothouber@gmail.com>
2020-06-15 01:49:00 +0200

    Add more desugar tests

--- src/test/kotlin/me/wollemat/sake/test/desugarer/SakeDesugarerExpressionTests.kt
+++ src/test/kotlin/me/wollemat/sake/test/desugarer/SakeDesugarerExpressionTests.kt
@@ -37,4 +37,88 @@ class SakeDesugarerExpressionTests {
             dast.toString()
         )
     }
+
+    @Test
+    fun `negation expression desugar test`() {
+        val src = "fun f() -> -2"
+
+        val dast = SakeDesugarer(SakeStringParser(src).parse()).desugar()
+
+        Assertions.assertEquals(
+            "${BASE}MultCore(left=IntegerCore(num=-1), right=IntegerCore(num=2))$END",
+            dast.toString()
+        )
+    }
+
+    @Test
+    fun `ge expression desugar test`() {
+        val src = "fun f() -> 1 >= 2"
+
+        val dast = SakeDesugarer(SakeStringParser(src).parse()).desugar()
+
+        Assertions.assertEquals(
+            "${BASE}NandCore(left=NandCore(left=LtCore(left=IntegerCore(num=2), right=IntegerCore(num=1)), right=LtCore(left=IntegerCore(num=2), right=IntegerCore(num=1))), right=NandCore(left=EqCore(left=IntegerCore(num=1), right=IntegerCore(num=2)), right=EqCore(left=IntegerCore(num=1), right=IntegerCore(num=2))))$END",
+            dast.toString()
+        )
+    }
+
+    @Test
+    fun `gt expression desugar test`() {
+        val src = "fun f() -> 1 > 2"
+
+        val dast = SakeDesugarer(SakeStringParser(src).parse()).desugar()
+
+        Assertions.assertEquals(
+            "${BASE}LtCore(left=IntegerCore(num=2), right=IntegerCore(num=1))$END",
+            dast.toString()
+        )
+    }
+
+    @Test
+    fun `le expression desugar test`() {
+        val src = "fun f() -> 1 <= 2"
+
+        val dast = SakeDesugarer(SakeStringParser(src).parse()).desugar()
+
+        Assertions.assertEquals(
+            "${BASE}NandCore(left=NandCore(left=LtCore(left=IntegerCore(num=1), right=IntegerCore(num=2)), right=LtCore(left=IntegerCore(num=1), right=IntegerCore(num=2))), right=NandCore(left=EqCore(left=IntegerCore(num=1), right=IntegerCore(num=2)), right=EqCore(left=IntegerCore(num=1), right=IntegerCore(num=2))))$END",
+            dast.toString()
+        )
+    }
+
+    @Test
+    fun `lt expression desugar test`() {
+        val src = "fun f() -> 1 < 2"
+
+        val dast = SakeDesugarer(SakeStringParser(src).parse()).desugar()
+
+        Assertions.assertEquals(
+            "${BASE}LtCore(left=IntegerCore(num=1), right=IntegerCore(num=2))$END",
+            dast.toString()
+        )
+    }
+
+    @Test
+    fun `eq expression desugar test`() {
+        val src = "fun f() -> 1 == 2"
+
+        val dast = SakeDesugarer(SakeStringParser(src).parse()).desugar()
+
+        Assertions.assertEquals(
+            "${BASE}EqCore(left=IntegerCore(num=1), right=IntegerCore(num=2))$END",
+            dast.toString()
+        )
+    }
+
+    @Test
+    fun `neq expression desugar test`() {
+        val src = "fun f() -> 1 != 2"
+
+        val dast = SakeDesugarer(SakeStringParser(src).parse()).desugar()
+
+        Assertions.assertEquals(
+            "${BASE}NandCore(left=EqCore(left=IntegerCore(num=1), right=IntegerCore(num=2)), right=EqCore(left=IntegerCore(num=1), right=IntegerCore(num=2)))$END",
+            dast.toString()
+        )
+    }
 }
