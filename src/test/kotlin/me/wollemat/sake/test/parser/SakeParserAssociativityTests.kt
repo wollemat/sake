7e0a40046763bf9702136f0ce15826c5398fafe5
wollemat <f.c.slothouber@gmail.com>
2020-06-15 15:54:31 +0200

    Add associativity tests

--- src/test/kotlin/me/wollemat/sake/test/parser/SakeParserAssociativityTests.kt
+++ src/test/kotlin/me/wollemat/sake/test/parser/SakeParserAssociativityTests.kt
@@ -1,4 +1,37 @@
 package me.wollemat.sake.test.parser
 
+import me.wollemat.sake.parser.SakeStringParser
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
 class SakeParserAssociativityTests {
+    @ParameterizedTest
+    @CsvSource(
+        "*",
+        "/",
+        "%",
+        "+",
+        "-",
+        "and",
+        "or"
+    )
+    fun `infix associativity test`(op: String) {
+        val srcDefault = src("x $op y $op z")
+        val srcGrouped = src("(x $op y) $op z")
+
+        Assertions.assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "-",
+        "not"
+    )
+    fun `prefix associativity test`(op: String) {
+        val srcDefault = src("$op x")
+        val srcGrouped = src("($op x)")
+
+        Assertions.assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
 }
--- src/test/kotlin/me/wollemat/sake/test/parser/SakeParserPrecedenceTests.kt
+++ src/test/kotlin/me/wollemat/sake/test/parser/SakeParserPrecedenceTests.kt
@@ -1,156 +1,247 @@
 package me.wollemat.sake.test.parser
 
+import me.wollemat.sake.parser.SakeStringParser
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
 class SakeParserPrecedenceTests {
-    // @ParameterizedTest
-    // @CsvSource(
-    //     "*",
-    //     "/",
-    //     "%",
-    //     "+",
-    //     "-",
-    //     ">=",
-    //     ">",
-    //     "<=",
-    //     "<",
-    //     "==",
-    //     "!=",
-    //     "and",
-    //     "or"
-    // )
-    // fun `grouping is higher than binary operator`(op: String) {
-    //     val expr = "x $op (y or z)"
-    //     val src = src(expr)
-    //
-    //     val ast = SakeStringParser(src).parse()
-    //
-    //     assertEquals(ast(expr(expr)), ast)
-    // }
-    //
-    // @ParameterizedTest
-    // @CsvSource(
-    //     "-",
-    //     "not"
-    // )
-    // fun `grouping is higher than unary operator`(op: String) {
-    //     val expr = "$op (x or y)"
-    //     val src = src(expr)
-    //
-    //     val ast = SakeStringParser(src).parse()
-    //
-    //     assertEquals(ast(expr(expr)), ast)
-    // }
-    //
-    // @ParameterizedTest
-    // @CsvSource(
-    //     "+",
-    //     "-",
-    //     ">=",
-    //     ">",
-    //     "<=",
-    //     "<",
-    //     "==",
-    //     "!=",
-    //     "and",
-    //     "or"
-    // )
-    // fun `multiplication, division and remainder are higher than binary operator`(op: String) {
-    //     val exprMult = "x $op y * z"
-    //     val exprDiv = "x $op y / z"
-    //     val exprRem = "x $op y % z"
-    //
-    //     val srcMult = src(exprMult)
-    //     val srcDiv = src(exprDiv)
-    //     val srcRem = src(exprRem)
-    //
-    //     val astMult = SakeStringParser(srcMult).parse()
-    //     val astDiv = SakeStringParser(srcDiv).parse()
-    //     val astRem = SakeStringParser(srcRem).parse()
-    //
-    //     assertEquals(ast(expr(exprMult)), astMult)
-    //     assertEquals(ast(expr(exprDiv)), astDiv)
-    //     assertEquals(ast(expr(exprRem)), astRem)
-    // }
-    //
-    // @ParameterizedTest
-    // @CsvSource(
-    //     ">=",
-    //     ">",
-    //     "<=",
-    //     "<",
-    //     "==",
-    //     "!=",
-    //     "and",
-    //     "or"
-    // )
-    // fun `addition and subtraction are higher than binary operator`(op: String) {
-    //     val exprAdd = "x $op y + z"
-    //     val exprSub = "x $op y - z"
-    //
-    //     val srcAdd = src(exprAdd)
-    //     val srcSub = src(exprSub)
-    //
-    //     val astAdd = SakeStringParser(srcAdd).parse()
-    //     val astSub = SakeStringParser(srcSub).parse()
-    //
-    //     assertEquals(ast(expr(exprAdd)), astAdd)
-    //     assertEquals(ast(expr(exprSub)), astSub)
-    // }
-    //
-    // @ParameterizedTest
-    // @CsvSource(
-    //     "==",
-    //     "!=",
-    //     "and",
-    //     "or"
-    // )
-    // fun `ge, gt, le and lt are higher than binary operator`(op: String) {
-    //     val exprGe = "x $op y >= z"
-    //     val exprGt = "x $op y > z"
-    //     val exprLe = "x $op y <= z"
-    //     val exprLt = "x $op y < z"
-    //
-    //     val srcGe = src(exprGe)
-    //     val srcGt = src(exprGt)
-    //     val srcLe = src(exprLe)
-    //     val srcLt = src(exprLt)
-    //
-    //     val astGe = SakeStringParser(srcGe).parse()
-    //     val astGt = SakeStringParser(srcGt).parse()
-    //     val astLe = SakeStringParser(srcLe).parse()
-    //     val astLt = SakeStringParser(srcLt).parse()
-    //
-    //     assertEquals(ast(expr(exprGe)), astGe)
-    //     assertEquals(ast(expr(exprGt)), astGt)
-    //     assertEquals(ast(expr(exprLe)), astLe)
-    //     assertEquals(ast(expr(exprLt)), astLt)
-    // }
-    //
-    // @ParameterizedTest
-    // @CsvSource(
-    //     "and",
-    //     "or"
-    // )
-    // fun `equality and inequality are higher than binary operator`(op: String) {
-    //     val exprEq = "x $op y == z"
-    //     val exprNeq = "x $op y != z"
-    //
-    //     val srcEq = src(exprEq)
-    //     val srcNeq = src(exprNeq)
-    //
-    //     val astEq = SakeStringParser(srcEq).parse()
-    //     val astNeq = SakeStringParser(srcNeq).parse()
-    //
-    //     assertEquals(ast(expr(exprEq)), astEq)
-    //     assertEquals(ast(expr(exprNeq)), astNeq)
-    // }
-    //
-    // @Test
-    // fun `and is higher than binary operator`() {
-    //     val expr = "x or y and z"
-    //     val src = src(expr)
-    //
-    //     val ast = SakeStringParser(src).parse()
-    //
-    //     assertEquals(ast(expr(expr)), ast)
-    // }
+    @Test
+    fun `grouping precedence test`() {
+        val srcDefault = src("1 + 2 * 3")
+        val srcGrouped = src("1 + (2 * 3)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "*",
+        "/",
+        "%",
+        "+",
+        "-",
+        ">=",
+        ">",
+        "<=",
+        "<",
+        "==",
+        "!=",
+        "and",
+        "or"
+    )
+    fun `negation precedence test`(op: String) {
+        val srcDefault = src("1 $op -2")
+        val srcGrouped = src("1 $op (-2)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "*",
+        "/",
+        "%",
+        "+",
+        "-",
+        ">=",
+        ">",
+        "<=",
+        "<",
+        "==",
+        "!=",
+        "and",
+        "or"
+    )
+    fun `not precedence test`(op: String) {
+        val srcDefault = src("1 $op not 2")
+        val srcGrouped = src("1 $op (not 2)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "+",
+        "-",
+        ">=",
+        ">",
+        "<=",
+        "<",
+        "==",
+        "!=",
+        "and",
+        "or"
+    )
+    fun `multiplication precedence test`(op: String) {
+        val srcDefault = src("1 $op 2 * 3")
+        val srcGrouped = src("1 $op (2 * 3)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "+",
+        "-",
+        ">=",
+        ">",
+        "<=",
+        "<",
+        "==",
+        "!=",
+        "and",
+        "or"
+    )
+    fun `division precedence test`(op: String) {
+        val srcDefault = src("1 $op 2 / 3")
+        val srcGrouped = src("1 $op (2 / 3)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "+",
+        "-",
+        ">=",
+        ">",
+        "<=",
+        "<",
+        "==",
+        "!=",
+        "and",
+        "or"
+    )
+    fun `remainder precedence test`(op: String) {
+        val srcDefault = src("1 $op 2 % 3")
+        val srcGrouped = src("1 $op (2 % 3)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        ">=",
+        ">",
+        "<=",
+        "<",
+        "==",
+        "!=",
+        "and",
+        "or"
+    )
+    fun `addition precedence test`(op: String) {
+        val srcDefault = src("1 $op 2 + 3")
+        val srcGrouped = src("1 $op (2 + 3)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        ">=",
+        ">",
+        "<=",
+        "<",
+        "==",
+        "!=",
+        "and",
+        "or"
+    )
+    fun `subtraction precedence test`(op: String) {
+        val srcDefault = src("1 $op 2 - 3")
+        val srcGrouped = src("1 $op (2 - 3)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "==",
+        "!=",
+        "and",
+        "or"
+    )
+    fun `ge precedence test`(op: String) {
+        val srcDefault = src("1 $op 2 >= 3")
+        val srcGrouped = src("1 $op (2 >= 3)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "==",
+        "!=",
+        "and",
+        "or"
+    )
+    fun `gt precedence test`(op: String) {
+        val srcDefault = src("1 $op 2 > 3")
+        val srcGrouped = src("1 $op (2 > 3)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "==",
+        "!=",
+        "and",
+        "or"
+    )
+    fun `le precedence test`(op: String) {
+        val srcDefault = src("1 $op 2 <= 3")
+        val srcGrouped = src("1 $op (2 <= 3)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "==",
+        "!=",
+        "and",
+        "or"
+    )
+    fun `lt precedence test`(op: String) {
+        val srcDefault = src("1 $op 2 < 3")
+        val srcGrouped = src("1 $op (2 < 3)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "and",
+        "or"
+    )
+    fun `eq precedence test`(op: String) {
+        val srcDefault = src("1 $op 2 == 3")
+        val srcGrouped = src("1 $op (2 == 3)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "and",
+        "or"
+    )
+    fun `neq precedence test`(op: String) {
+        val srcDefault = src("1 $op 2 != 3")
+        val srcGrouped = src("1 $op (2 != 3)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
+
+    @Test
+    fun `and precedence test`() {
+        val srcDefault = src("1 or 2 and 3")
+        val srcGrouped = src("1 or (2 and 3)")
+
+        assertEquals(SakeStringParser(srcGrouped).parse(), SakeStringParser(srcDefault).parse())
+    }
 }
